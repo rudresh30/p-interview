9532dfd0ab051f23339f5fa57a2353f30aa531b7
rudresh30 <rudresh.pandya@gmail.com>
2018-10-12 12:38:15 +0530

    change port for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
